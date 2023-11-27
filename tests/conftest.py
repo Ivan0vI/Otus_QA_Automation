118e6507d3abd376b1aaa9949b2c1bb2b67da17a
ivanivanov <jantelman_ivan@mail.ru>
2023-11-27 18:01:07 +0300

    Add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,15 +6,18 @@ def params_circle():
     print("\nВвод данных для построения круга")
     yield 1
 
+
 @pytest.fixture(scope="session")
 def params_rectangle():
     print("\nВвод данных для построения треугольника")
     yield 5, 6, 30
 
+
 @pytest.fixture(scope="session")
 def params_square():
     print("\nВвод данных для построения квадрата")
     yield 5, 10
+
 
 @pytest.fixture(scope="session")
 def params_triangle():
